3d0919789f758310c19b3c175785d78cf98519f7
super shang <shangshipei@163.com>
2014-01-28 21:34:42 +0800

    README for this priject.

---
+++ a.c
@@ -0,0 +1,31 @@
+/**
+=========================================================================
+ Author: shangshipe
+ Email: shangshipei@163.com
+ File Name: a.c
+ Description: (C)  2013-05  findstr
+   
+ Edit History: 
+   2013-05-15    File created.
+=========================================================================
+**/
+int calendar[12][31];
+int calenda1r[12][31];
+int (*monthp)[31];
+int a;
+int b;
+int c;
+int j;
+int r;
+int e;
+int d;
+int k;
+main()
+{
+        for (monthp = calendar; monthp < &calendar[12]; monthp++){
+                int *dayp;
+                for (dayp = *monthp; dayp<&(*monthp)[31]; dayp++)
+                        *dayp = 0;
+        }
+}
+                
